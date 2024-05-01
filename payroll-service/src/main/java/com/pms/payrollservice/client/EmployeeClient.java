93beb55346382ac5e170c2c67d2209a6d1b69ce7
thenujad <dulwanat@gmail.com>
2024-05-01 22:08:30 +0530

    updated db

--- payroll-service/src/main/java/com/pms/payrollservice/client/EmployeeClient.java
+++ payroll-service/src/main/java/com/pms/payrollservice/client/EmployeeClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "employee-service", url = "http://localhost:8072/api/pms/employee")
+@FeignClient(name = "employee-service", url = "http://localhost:8072/employee")
 public interface EmployeeClient {
 
     @GetMapping("/{id}")
