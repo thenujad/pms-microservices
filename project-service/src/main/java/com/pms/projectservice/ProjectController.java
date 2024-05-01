2ee85fe6b6962c44c9c9e0252a8495db92fd13b9
thenujad <dulwanat@gmail.com>
2024-05-01 18:49:26 +0530

    updated config

--- project-service/src/main/java/com/pms/projectservice/ProjectController.java
+++ project-service/src/main/java/com/pms/projectservice/ProjectController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/projects")
+@RequestMapping("/project")
 public class ProjectController {
 
     @Autowired
